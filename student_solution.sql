b386984e231700d881971fcd98c5257991d2b755
premalathas1 <premalathas@rvsgroup.com>
2026-09-17 13:55:45 +0530

    Create Student table with relevant fields

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT NOT NULL
+);
