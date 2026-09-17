d897de86559aa75454d08b4e651b6ddecca119fc
premalathas1 <premalathas@rvsgroup.com>
2026-09-17 14:25:02 +0530

    Create starter.sql for Student table assignment
    Added SQL script to create a Student table with specified fields.

---
+++ starter.sql
@@ -0,0 +1,17 @@
+```sql
+-- Student Table Assignment
+--
+-- Create a table named Student with the following fields:
+--
+-- StudentID     - NUMBER(5)    - PRIMARY KEY
+-- StudentName   - VARCHAR(20)  - NOT NULL, UNIQUE
+-- DOB           - DATE         - NOT NULL
+-- Gender        - VARCHAR(10)  - NOT NULL
+-- DepartmentID  - NUMBER(5)    - NOT NULL
+--
+-- Write your CREATE TABLE statement below.
+
+DROP TABLE IF EXISTS Student;
+
+-- WRITE YOUR SQL CODE HERE
+```
